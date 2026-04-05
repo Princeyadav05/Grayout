a258fe36128a73545240b1525a1da043513a78fa
Prince Yadav <princeyadav96@gmail.com>
2026-04-05 23:20:08 +0530

    feat(service): add GrayscaleManager for daltonizer setting read/write

---
+++ app/src/main/java/com/princeyadav/grayout/service/GrayscaleManager.kt
@@ -0,0 +1,26 @@
+package com.princeyadav.grayout.service
+
+import android.content.ContentResolver
+import android.provider.Settings
+
+class GrayscaleManager(private val contentResolver: ContentResolver) {
+
+    fun isGrayscaleEnabled(): Boolean {
+        return Settings.Secure.getInt(contentResolver, DALTONIZER_ENABLED, 0) == 1
+    }
+
+    fun setGrayscale(enabled: Boolean) {
+        if (enabled) {
+            Settings.Secure.putInt(contentResolver, DALTONIZER_ENABLED, 1)
+            Settings.Secure.putInt(contentResolver, DALTONIZER_MODE, 0)
+        } else {
+            Settings.Secure.putInt(contentResolver, DALTONIZER_ENABLED, 0)
+            Settings.Secure.putInt(contentResolver, DALTONIZER_MODE, -1)
+        }
+    }
+
+    companion object {
+        private const val DALTONIZER_ENABLED = "accessibility_display_daltonizer_enabled"
+        private const val DALTONIZER_MODE = "accessibility_display_daltonizer"
+    }
+}
