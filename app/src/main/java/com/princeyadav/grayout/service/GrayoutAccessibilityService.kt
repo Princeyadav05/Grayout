87e4cb2e62d345aad9ea3893bafb65da7102e245
Prince Yadav <princeyadav96@gmail.com>
2026-04-06 12:29:40 +0530

    fix(service): restore grayscale based on saved state, not enforcement interval

--- app/src/main/java/com/princeyadav/grayout/service/GrayoutAccessibilityService.kt
+++ app/src/main/java/com/princeyadav/grayout/service/GrayoutAccessibilityService.kt
@@ -6,14 +6,12 @@ import android.view.accessibility.AccessibilityEvent
 class GrayoutAccessibilityService : AccessibilityService() {
 
     private lateinit var exclusionPrefs: ExclusionPrefs
-    private lateinit var enforcementPrefs: EnforcementPrefs
     private lateinit var grayscaleManager: GrayscaleManager
 
     override fun onCreate() {
         super.onCreate()
         val prefs = getSharedPreferences(EnforcementPrefs.PREFS_NAME, MODE_PRIVATE)
         exclusionPrefs = ExclusionPrefs(prefs)
-        enforcementPrefs = EnforcementPrefs(prefs)
         grayscaleManager = GrayscaleManager(contentResolver)
     }
 
@@ -30,11 +28,14 @@ class GrayoutAccessibilityService : AccessibilityService() {
         val wasActive = exclusionPrefs.isExcludedAppActive()
 
         if (isExcluded && !wasActive) {
+            exclusionPrefs.setWasGrayscaleOnBeforeExclusion(grayscaleManager.isGrayscaleEnabled())
             exclusionPrefs.setExcludedAppActive(true)
             grayscaleManager.setGrayscale(false)
         } else if (!isExcluded && wasActive) {
+            val wasOn = exclusionPrefs.wasGrayscaleOnBeforeExclusion()
             exclusionPrefs.setExcludedAppActive(false)
-            if (enforcementPrefs.getInterval() > 0) {
+            exclusionPrefs.setWasGrayscaleOnBeforeExclusion(false)
+            if (wasOn) {
                 grayscaleManager.setGrayscale(true)
             }
         }
