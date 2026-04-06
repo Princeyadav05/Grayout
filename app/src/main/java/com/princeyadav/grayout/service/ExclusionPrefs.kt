3af988cecdae61cffeeb21e94744f156200325cd
Prince Yadav <princeyadav96@gmail.com>
2026-04-06 12:26:52 +0530

    feat(service): add grayscale-state pref to ExclusionPrefs

--- app/src/main/java/com/princeyadav/grayout/service/ExclusionPrefs.kt
+++ app/src/main/java/com/princeyadav/grayout/service/ExclusionPrefs.kt
@@ -35,8 +35,16 @@ class ExclusionPrefs(private val prefs: SharedPreferences) {
 
     fun getExcludedCount(): Int = getExcludedPackages().size
 
+    fun wasGrayscaleOnBeforeExclusion(): Boolean =
+        prefs.getBoolean(KEY_WAS_GRAYSCALE_ON, false)
+
+    fun setWasGrayscaleOnBeforeExclusion(wasOn: Boolean) {
+        prefs.edit().putBoolean(KEY_WAS_GRAYSCALE_ON, wasOn).apply()
+    }
+
     companion object {
         private const val KEY_EXCLUDED_PACKAGES = "excluded_packages"
         private const val KEY_EXCLUDED_APP_ACTIVE = "excluded_app_active"
+        private const val KEY_WAS_GRAYSCALE_ON = "was_grayscale_on_before_exclusion"
     }
 }
